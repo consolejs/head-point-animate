c4810e1756048576882c0e40386fbb6efa128d1c
consolejs <likun205743@sogou-inc.com>
2021-09-07 15:26:39 +0800

    :bug: fix:  定位

--- src/App.js
+++ src/App.js
@@ -3,14 +3,27 @@ import "./index.css";
 import { HeadPoint } from "./components/headPoint";
 
 export default function App() {
+
+  const userIno = {
+    src:
+      "https://i.picsum.photos/id/603/50/50.jpg?hmac=_USXyljqpKjZi8WyEyPYPDm9bjzp_mHNUw" +
+      "h6goOkQG8",
+    number: "2123",
+    nickname: "zhangsan",
+    posX: "50%",
+    posY: "50%",
+    posW: "200px",
+    posH: "160px",
+  }
+
   return (
     <div className="App">
-      <HeadPoint />
-      {/* <div className="lightup-btn">
+      <HeadPoint userIno={userIno} />
+      <div className="lightup-btn">
         <span  className="tx">
           点亮画卷
         </span>
-      </div> */}
+      </div>
     </div>
   );
 }
--- src/components/headPoint/index.js
+++ src/components/headPoint/index.js
@@ -1,56 +1,69 @@
-import { useState } from "react";
-
+import {useState, useEffect} from "react";
+import {Zoom, Bounce, Pulse} from "@gfazioli/react-animatecss";
 import styles from "./index.module.css";
-import { Zoom, Bounce, Pulse } from "@gfazioli/react-animatecss";
-
-const getHeadPoint = (info) => {
-  return (
-    <div
-      style={{
-        top: info.posY + "px",
-        left: info.posX + "px",
-      }}
-      className={styles.head_point}
-    >
-      <div className={styles.cnt}>
-        <div className={styles.pic}>
-          <img src={info.src} alt="" />
-        </div>
-        <div className={styles.name}>{info.nickname}</div>
-        <div className={styles.desc}>
-          你是第<b>{info.number}</b>位点亮画卷，
-          <br />
-          欢迎一起回家
-        </div>
-      </div>
-    </div>
-  );
-};
 
 export const HeadPoint = (props) => {
+
+  const { userIno } = props;
+
+  const [info, setInfo] = useState(userIno);
 
   const [startOne, setStartOne] = useState(false);
   const [startTwo, setStartTwo] = useState(false);
   const [startThree, setStartThree] = useState(false);
-  const [starEnd, setStarEnd] = useState(false);
+  
 
-  const info = {
-    src: "https://i.picsum.photos/id/603/50/50.jpg?hmac=_USXyljqpKjZi8WyEyPYPDm9bjzp_mHNUwh6goOkQG8",
-    number: "2123",
-    nickname: "zhangsan",
-    posX: 330,
-    posY: 80,
-  };
+  useEffect(() => {
+    setStartOne(true);
+  }, [props]);
 
   const showPic = () => {
-    console.log('截图开始~')
+    console.log("截图开始~");
+  };
+
+  const resPos = () => {
+    console.log("改变位置");
+    setInfo({
+      src: info.src,
+      number: info.number,
+      nickname: info.nickname,
+      posX: "32px",
+      posY: "32px",
+      posW: "24px",
+      posH: "24px"
+    })
+    setTimeout(() => {
+      showPic();
+    }, 2000)
   }
 
-  return (
-    <div className="head-point_wrap">
-      
-      <button onClick={() => setStartOne(true)}>点亮</button>
+  const getHeadPoint = (info) => {
+    return (
+      <div className={styles.head_point}>
+        <div className={styles.cnt}>
+          <div className={styles.pic}>
+            <img src={info.src} alt=""/>
+          </div>
+          <div className={styles.name}>{info.nickname}</div>
+          <div className={styles.desc}>
+            你是第<b>{info.number}</b>位点亮画卷，
+            <br/>
+            欢迎一起回家
+          </div>
+        </div>
+      </div>
+    );
+  };
 
+  return (
+    <div
+      className={styles.wrap}
+      style={{
+      top: info.posY,
+      left: info.posX,
+      width: info.posW,
+      height: info.posH
+    }}>
       <Zoom
         animate={startOne}
         direction="down"
@@ -58,41 +71,26 @@ export const HeadPoint = (props) => {
         mode="in"
         as="div"
         block
-        onAnimationEnd={() => setStartTwo(true)}
-      >
+        onAnimationEnd={() => setStartTwo(true)}>
         <Bounce
           animate={startTwo}
           duration="1.5s"
           repeat="1"
           onAnimationEnd={() => setStartThree(true)}
-          as="div"
-        >
+          as="div">
           <Pulse
             animate={startThree}
             duration="1.2s"
             repeat="2"
-            onAnimationEnd={() => setStarEnd(true)}
-            as="div"
-          >
-               <Zoom
-                animate={starEnd}
-                mode="out"
-                delay="1s"
-                duration="2s"
-                as="div"
-                block
-                onAnimationEnd={()=>{
-                  showPic()
-                }}
-              >
-                {getHeadPoint(info)}
-              </Zoom>
-              
+            onAnimationEnd={() => {
+            setStartThree(false);
+            resPos();
+          }}
+            as="div">
+            {getHeadPoint(info)}
           </Pulse>
         </Bounce>
       </Zoom>
-
-     
     </div>
   );
 };
